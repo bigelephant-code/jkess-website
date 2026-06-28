6118e64df398f1f3ffb654615026fbf6eaa65970
bigelephant-code <13162828868@163.com>
2026-06-28 21:37:22 +0800

    Restore hero dependency

---
+++ src/components/HeroSection2.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { ArrowRight } from 'lucide-react'
+import AnimatedBackground from './AnimatedBackground'
+import { motion } from 'framer-motion'
+import { useI18n } from '@/i18n/client'
+import { localizedPath } from '@/lib/lang'
+
+export default function HeroSection2() {
+  return <AnimatedBackground />
+}
